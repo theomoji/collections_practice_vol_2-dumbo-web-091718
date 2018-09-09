13caec6a1aab15b064a2ef938b26a7dd80604e57
Shuo Wang <gken90@gmail.com>
2018-09-09 17:50:10 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
-# your code goes here
+def begins_with_r(array)
+  if array.all? {|word| (word.start_with?("r")) == true}
+    return true
+  elsif array.any? {|word| (word.start_with?("r")) == false}
+    return false
+  else
+    false
+  end
+end
+
+def contain_a(array)
+  array.select {|word| word.include?("a") }
+end
+
+def first_wa(array)
+  array.each {|word| return word if word[0..1] == "wa" }
+end
+
+def remove_non_strings(array)
+  array.select {|item| item.is_a? String }
+end
+
+def count_elements(array)
+
+end 
